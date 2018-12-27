f3015f2b2b5ee1d0f6b8cfda1d4f2ab345bf0ac7
gejun <917609824@qq.com>
2018-12-28 01:26:47 +0800

    add retrofit demo

---
+++ Samples/retrofit/src/main/java/com/ddd/retrofit/MainActivity.java
@@ -0,0 +1,52 @@
+package com.ddd.retrofit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView html;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        html =(TextView)findViewById(R.id.html);
+
+        //创建Retrofit对象
+        Retrofit retrofit = new Retrofit.Builder()
+                //指定baseurl，这里有坑，最后后缀出带着“/”
+                .baseUrl("http://www.baidu.com/")
+                //设置内容格式,这种对应的数据返回值是String类型
+                .addConverterFactory(ScalarsConverterFactory.create())
+                //定义client类型
+                .client(new OkHttpClient())
+                //创建
+                .build();
+        //通过retrofit和定义的有网络访问方法的接口关联
+        BaiduApi dataService = retrofit.create(BaiduApi.class);
+        //在这里又重新设定了一下baidu的地址，是因为Retrofit要求传入具体，如果是决定路径的话，路径会将baseUrl覆盖掉
+        Call<String> baidu = dataService.getHtml("http://wwww.baidu.com");
+        //执行异步请求
+        baidu.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+//                Toast.makeText(MainActivity.this,  response.body(), Toast.LENGTH_SHORT).show();
+                html.setText(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
+    }
+}
